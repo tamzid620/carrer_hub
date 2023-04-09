8d485cc35bab8df167afc822d691894ddcfea197
tamzid620 <tamzid620@gmail.com>
2023-04-09 23:44:29 +0600

    dp section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+{/* DP section------------------ */}
+            <div  className='flex items-center justify-center'>
+                <div>
+                    <h1 className='text-7xl text-left font-semibold mb-5'>One Step <br /> Closer To Your <br /> <span className='text-pink-600'> Dream Job</span></h1>
+                    <p className='text-gray-400 text-start'>A highly creative thinker, grammar Nazi, and social media <br /> enthusiast seek the position of Social Media & <br /> Content Marketing Analyst to transform technical <br /> and digital information and processes into influencial stories.</p>
+                </div>
+                <div className='dp'>
+                    <img src="../../../assets/All Images/P3OLGJ1 copy 1.png" alt="" />
+                </div>
+            </div>
+{/* Main section ---------------------- */}
+            <div></div>
         </div>
     );
-};
+}
 
 export default Home;
